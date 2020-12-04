26b74dff2332f191b58b94e26c65a07e749e1bfc
gscarborough <gigimscarborough@gmail.com>
2020-12-04 17:28:05 -0500

    merged forms added to controller

--- 99Cats/app/controllers/cats_controller.rb
+++ 99Cats/app/controllers/cats_controller.rb
@@ -11,4 +11,16 @@ class CatsController < ApplicationController
         render :show
     end
 
+    def new
+        @cat = Cat.new
+
+        render :new
+    end
+
+    def edit
+        @cat = Cat.find(params[:id])
+
+        render :edit
+    end
+
 end
