075591aff2c7cd8fc23517ed80bcd2a0bb125adc
gscarborough <gigimscarborough@gmail.com>
2020-12-04 14:31:00 -0500

    created models/controllers

--- 99Cats/app/models/cat.rb
+++ 99Cats/app/models/cat.rb
@@ -1,7 +1,15 @@
+require "action_view"
+
 class Cat < ApplicationRecord
+    
+    include ActionView::Helpers::DateHelper
+
     COLORS = ['Gray', 'Black', 'Orange', 'White', 'Brown', 'Yellow', 'Rainbow']
     validates :color, inclusion: {in: COLORS}
     validates :sex, inclusion: {in: ["M","F"]}
     validates :name, :sex, :color, :age, :description, presence: true
     
+    def age
+        time_ago_in_words(birth_date)
+    end
 end
